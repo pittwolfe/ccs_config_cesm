e64d9008d88d488533dcdeb532aa4d0b5b765e37
Christopher Pitt Wolfe <christopher.wolfe@stonybrook.edu>
2026-03-24 12:01:43 +0000

    Add cmake macros for Archer2

--- machines/archer2/gnu_archer2.cmake
+++ machines/archer2/gnu_archer2.cmake
@@ -1,1 +1,8 @@
-# It's possible we don't need anything specific here
+# It's possible we don't need anything specific hereset(CMAKE_OPTS "-DCMAKE_SYSTEM_NAME=Catamount")
+string(APPEND CPPDEFS " -DLINUX")
+if (COMP_NAME STREQUAL gptl)
+  string(APPEND CPPDEFS " -DHAVE_NANOTIME -DBIT64 -DHAVE_VPRINTF -DHAVE_BACKTRACE -DHAVE_SLASHPROC -DHAVE_COMM_F2C -DHAVE_TIMES -DHAVE_GETTIMEOFDAY")
+endif()
+set(NETCDF_PATH "$ENV{NETCDF_DIR}")
+set(PIO_FILESYSTEM_HINTS "lustre")
+set(PNETCDF_PATH "$ENV{PARALLEL_NETCDF_DIR}")
